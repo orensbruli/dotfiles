6933f051a516ae0ba007ab4a7741d285e3f7889b
Esteban Martinena <orensbruli@gmail.com>
2024-09-11 12:15:51 +0200

    run_once

---
+++ run_once_bootstrap.sh
@@ -0,0 +1,11 @@
+sudo apt update
+
+# Install ansible
+if ! command -v ansible-playbook &> /dev/null; then
+    sudo apt install -y ansible
+fi
+
+if command -v ansible-playbook &> /dev/null; then
+    ansible-galaxy install -r {{ joinPath .chezmoi.sourceDir "dot_bootstrap/requirements.yaml"}}
+    ansible-playbook -v {{ joinPath .chezmoi.sourceDir "dot_bootstrap/setup.yaml" | quote }} --ask-become-pass
+fi
